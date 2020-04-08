b5f40392aca70178a5d8ecf87972d0364e2dda53
Yingming Luo <Yingming.luo@gmail.com>
2020-04-09 00:37:39 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Abid to the social distancing guidlines, protect NHS and save lives!</Text>
+      <Text>Press button to start tracking the safe distance between you and others.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
