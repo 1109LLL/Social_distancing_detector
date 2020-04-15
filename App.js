ab904deaec6d17e5b3e44448813663d8973da861
Yingming Luo <Yingming.luo@gmail.com>
2020-04-16 00:41:54 +0100

    fixed Alert not changing. bug: component does not re-render properly, need to be fixed

--- App.js
+++ App.js
@@ -1,36 +1,29 @@
 import React, { useState, Component } from 'react';
 import { Button, StyleSheet, Text, View, Alert } from 'react-native';
 
-
-// const [buttonState, setButtonState] = useState(false);
-let buttonState = false;
-
-activateButton = () =>
-{
-Alert.alert('Detector Activated')
-}
-
-deactivateButton = () =>
-{
-Alert.alert('Detector Deactivated!')
-}
-
-function alertHandler()
-{
-  // setButtonState(!buttonState);
-
-  if (buttonState)
-  {
-    return activateButton;
-  }
-  else
-  {
-    return deactivateButton;
-  }
-}
-
-
 export default function App() {
+
+  let buttonState = false;
+  const [_title, _setTitle] = useState('Start')
+
+  function alertHandler()
+  {
+    buttonState = !buttonState;
+
+    console.log('The buttonState :: '+buttonState);
+    if (buttonState == true)
+    {
+      console.log('It is true');
+      _setTitle('Stop');
+      return Alert.alert('Detector Activated');
+    }
+    else
+    {
+      console.log('It is false');
+      _setTitle('Start');
+      return Alert.alert('Detector Deactivated!');
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -38,9 +31,9 @@ export default function App() {
         Please press button to activate detector
       </Text>
       <Button
-        title = "Start"
+        title = {_title}
         color ="#f194ff"
-        onPress={alertHandler()}
+        onPress={() => alertHandler()}
       />
     </View>
   );
